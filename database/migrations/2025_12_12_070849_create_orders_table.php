93cb7790416d2bfbd1568598bba593709ef464be
kenneyher <kenneyher@gmail.com>
2025-12-12 03:57:39 -0600

    ADD: migrations for addresses and orders tables

---
+++ database/migrations/2025_12_12_070849_create_orders_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use App\Enums\OrderStatus;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+
+            // Status Management
+            $table->enum('status', array_column(OrderStatus::cases(), 'value'))->default(OrderStatus::PENDING->value);
+            $table->string('payment_method')->default('credit_card');
+
+            // Financials
+            $table->decimal('subtotal', 10, 2);
+            $table->decimal('shipping_cost', 10, 2)->default(0);
+
+            // Address Snapshots
+            $table->json('shipping_address');
+            $table->json('billing_address');
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
